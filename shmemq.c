5210d468ac91ec844e35c17d51d0c38dcf3ba565
VintagePC <53943260+vintagepc@users.noreply.github.com>
2021-02-06 12:19:25 -0500

    Customize for MK404

--- shmemq.c
+++ shmemq.c
@@ -16,123 +16,137 @@
 #include "shmemq.h"
 
 struct shmemq_info {
-  pthread_mutex_t lock;
-  unsigned long read_index;
-  unsigned long write_index;
-  char data[1];
+	pthread_mutex_t lock;
+	unsigned long read_index;
+	unsigned long write_index;
+	char data[1];
 };
 
 struct _shmemq {
-  unsigned long max_count;
-  unsigned int element_size;
-  unsigned long max_size;
-  char* name;
-  int shmem_fd;
-  unsigned long mmap_size;
-  struct shmemq_info* mem;
+	unsigned long max_count;
+	unsigned int element_size;
+	unsigned long max_size;
+	char* name;
+	int shmem_fd;
+	unsigned long mmap_size;
+	struct shmemq_info* mem;
 };
 
-shmemq_t* shmemq_new(char const* name, unsigned long max_count, unsigned int element_size) {
-  shmemq_t* self;
-  bool created;
+shmemq_t* _shmemq_new(const char* name, bool bConsumer);
 
-  self = (shmemq_t*)malloc(sizeof(shmemq_t));
-  self->max_count = max_count;
-  self->element_size = element_size;
-  self->max_size = max_count * element_size;
-  self->name = strdup(name);
-  self->mmap_size = self->max_size + sizeof(struct shmemq_info) - 1;
+shmemq_t* _shmemq_new(const char* name, bool bConsumer)
+{
+	shmemq_t* self;
+	bool created;
+	unsigned int max_count = SHMQUEUE_MAX_COUNT;
+	unsigned int element_size = SHMQUEUE_MSG_LEN;
+	self = (shmemq_t*)malloc(sizeof(shmemq_t));
+	self->max_count = max_count;
+	self->element_size = element_size;
+	self->max_size = max_count * element_size;
+	self->name = strdup(name);
+	self->mmap_size = self->max_size + sizeof(struct shmemq_info) - 1;
 
-  created = false;
-  self->shmem_fd = shm_open(name, O_RDWR, S_IRUSR | S_IWUSR);
-  if (self->shmem_fd == -1) {
-    if (errno == ENOENT) {
-      self->shmem_fd = shm_open(name, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-      if (self->shmem_fd == -1) {
-        goto FAIL;
-      }
-      created = true;
-    } else {
-      goto FAIL;
+	created = false;
+	if (bConsumer) {
+		self->shmem_fd = shm_open(name, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+		if (self->shmem_fd == -1) {
+            perror(name);
+			goto FAIL;
+		}
+		created = true;
+	} else {
+		self->shmem_fd = shm_open(name, O_RDWR, S_IRUSR | S_IWUSR);
+		if (self->shmem_fd == -1) {
+            perror(name);
+			goto FAIL;
+		}
+	}
+	printf("initialized queue %s, created = %d\n", name, created);
+	if (created && (-1 == ftruncate(self->shmem_fd, self->mmap_size)))
+    {
+	    goto FAIL;
     }
-  }
-  printf("initialized queue %s, created = %d\n", name, created);
-  if (created && (-1 == ftruncate(self->shmem_fd, self->mmap_size)))
-    goto FAIL;
+    self->mem = (struct shmemq_info*)mmap(NULL, self->mmap_size, PROT_READ | PROT_WRITE, MAP_SHARED, self->shmem_fd, 0);
 
-  self->mem = (struct shmemq_info*)mmap(NULL, self->mmap_size, PROT_READ | PROT_WRITE, MAP_SHARED, self->shmem_fd, 0);
-  if (self->mem == MAP_FAILED)
-    goto FAIL;
 
-  if (created) {
-    self->mem->read_index = self->mem->write_index = 0;
-    pthread_mutexattr_t attr;
-    pthread_mutexattr_init(&attr);
-    pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED);
+	if (self->mem == MAP_FAILED)
+	goto FAIL;
+
+	if (created) {
+	self->mem->read_index = self->mem->write_index = 0;
+	pthread_mutexattr_t attr;
+	pthread_mutexattr_init(&attr);
+	pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED);
 #if ADAPTIVE_MUTEX
-    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ADAPTIVE_NP);
+	pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ADAPTIVE_NP);
 #endif
-    pthread_mutex_init(&self->mem->lock, &attr);
-    pthread_mutexattr_destroy(&attr);
-    // TODO Need to clean up the mutex? Also, maybe mark it as robust? (pthread_mutexattr_setrobust)
-  }
+	pthread_mutex_init(&self->mem->lock, &attr);
+	pthread_mutexattr_destroy(&attr);
+	// TODO Need to clean up the mutex? Also, maybe mark it as robust? (pthread_mutexattr_setrobust)
+	}
 
-  return self;
+	return self;
 
 FAIL:
-  if (self->shmem_fd != -1) {
-    close(self->shmem_fd);
-    shm_unlink(self->name);
-  }
-  free(self->name);
-  free(self);
-  return NULL;
+	if (self->shmem_fd != -1) {
+		close(self->shmem_fd);
+		//shm_unlink(self->name);
+	}
+	free(self->name);
+	free(self);
+	return NULL;
 }
 
-bool shmemq_try_enqueue(shmemq_t* self, void* element, int len) {
-  if (len != self->element_size)
-    return false;
-
-  pthread_mutex_lock(&self->mem->lock);
-
-  // TODO this test needs to take overflow into account  
-  if (self->mem->write_index - self->mem->read_index >= self->max_size) {
-    pthread_mutex_unlock(&self->mem->lock);
-    return false; // There is no more room in the queue
-  }
-
-  memcpy(&self->mem->data[self->mem->write_index % self->max_size], element, len);
-  self->mem->write_index += self->element_size;
-
-  pthread_mutex_unlock(&self->mem->lock);
-  return true;
+shmemq_t* shmemq_create(char const* name) {
+	return _shmemq_new(name, true);
 }
 
-bool shmemq_try_dequeue(shmemq_t* self, void* element, int len) {
-  if (len != self->element_size)
-    return false;
+shmemq_t* shmemq_open(const char* name) {
+	return _shmemq_new(name,false);
+}
 
-  pthread_mutex_lock(&self->mem->lock); 
 
-  // TODO this test needs to take overflow into account
-  if (self->mem->read_index >= self->mem->write_index) {
-    pthread_mutex_unlock(&self->mem->lock);
-    return false; // There are no elements that haven't been consumed yet
-  }
- 
-  memcpy(element, &self->mem->data[self->mem->read_index % self->max_size], len);
-  self->mem->read_index += self->element_size;
+bool shmemq_try_enqueue(shmemq_t* self, shm404_msg_t* element) {
 
-  pthread_mutex_unlock(&self->mem->lock);
-  return true;
-} 
+	pthread_mutex_lock(&self->mem->lock);
+
+	// TODO this test needs to take overflow into account
+	if (self->mem->write_index - self->mem->read_index >= self->max_size) {
+		pthread_mutex_unlock(&self->mem->lock);
+		return false; // There is no more room in the queue
+	}
+
+	memcpy(&self->mem->data[self->mem->write_index % self->max_size], element, SHMQUEUE_MSG_LEN);
+	self->mem->write_index += self->element_size;
+
+	pthread_mutex_unlock(&self->mem->lock);
+	return true;
+}
+
+bool shmemq_try_dequeue(shmemq_t* self, shm404_msg_t* element) {
+
+	pthread_mutex_lock(&self->mem->lock);
+
+	// TODO this test needs to take overflow into account
+	if (self->mem->read_index >= self->mem->write_index) {
+		pthread_mutex_unlock(&self->mem->lock);
+		return false; // There are no elements that haven't been consumed yet
+	}
+
+	memcpy(element, &self->mem->data[self->mem->read_index % self->max_size], SHMQUEUE_MSG_LEN);
+	self->mem->read_index += self->element_size;
+
+	pthread_mutex_unlock(&self->mem->lock);
+	return true;
+}
 
 void shmemq_destroy(shmemq_t* self, int unlink) {
-  munmap(self->mem, self->max_size);
-  close(self->shmem_fd);
-  if (unlink) {
-    shm_unlink(self->name);
-  }
-  free(self->name);
-  free(self);
+	munmap(self->mem, self->max_size);
+	close(self->shmem_fd);
+	if (unlink) {
+		shm_unlink(self->name);
+	}
+	free(self->name);
+	free(self);
 }
--- shmemq.h
+++ shmemq.h
@@ -1,8 +1,39 @@
 #include <stdbool.h>
 
+#ifndef SHMEMQ_H
+#define SHMEMQ_H
+
 typedef struct _shmemq shmemq_t;
 
-shmemq_t* shmemq_new(char const* name, unsigned long max_count, unsigned int element_size);
-bool shmemq_try_enqueue(shmemq_t* self, void* element, int len);
-bool shmemq_try_dequeue(shmemq_t* self, void* element, int len);
+#define SHMQUEUE_MAX_COUNT 500u
+#define SHMQUEUE_MSG_LEN   7u
+
+#define ADAPTIVE_MUTEX 1
+
+typedef char shm404_msg_t[SHMQUEUE_MSG_LEN];
+
+#define SHM_CLEAR {'C',0,0,0,0,0,0}
+
+#define SHM_ADD_MOTOR       {'A','M',0,0,0,0,0}
+#define SHM_SET_MOTOR_ID    {'M','0','L',0,0,0,0}
+#define SHM_SET_MOTOR_SIMPLE {'M','0','S',0,0,0,0}
+#define SHM_SET_MOTOR_USTEPS {'M','0','U',0,0,0,0}
+#define SHM_SET_MOTOR_MSTEPS {'M','0','X',0,0,0,0}
+#define SHM_SET_MOTOR_SPOS  {'M','0','P',0,0,0,0}
+#define SHM_SET_MOTOR_EN  {'M','0','E',0,0,0,0}
+
+#define SHM_ADD_INDICATOR   {'A','I',0,0,0,0,0}
+#define SHM_SET_INDICATOR   {'I','0','V',0,0,0,0}
+#define SHM_SET_IND_COLOUR  {'I','0','C',0,0,0,0}
+
+
+// Creates the new queue for the "consumer" side
+shmemq_t* shmemq_create(char const* name);
+// Only opens an existing queue, does not create it. 
+shmemq_t* shmemq_open(char const* name);
+
+bool shmemq_try_enqueue(shmemq_t* self, shm404_msg_t* element);
+bool shmemq_try_dequeue(shmemq_t* self, shm404_msg_t* element);
 void shmemq_destroy(shmemq_t* self, int unlink); 
+
+#endif
